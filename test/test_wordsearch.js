7131f1272036bbd68dde49c78577725c0bf38665
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-01-18 20:20:56 -0800

    wordsearch and tests - pair programming with @Colton Hyland

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,16 +1,34 @@
+
+
 const chai = require('chai');
 const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'R', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'A', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'K', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+
   it("should return false if the word is not present", function() {
     const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['A', 'D', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'R', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'A', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'K', 'C', 'S', 'Y', 'E', 'R', 'L'],
       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
+
 const wordSearch = (letters, word) => { 
+    if(letters.length < 1) {
+      return false
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let arr = []
+    for (let x = 0; x < letters[0].length; x++) {
+        arr.push([]);
+    }
+    for (let i = 0; i < letters.length; i++) {
+        for (let j = 0; j < letters[i].length; j++) {
+            arr[j][i] = letters[i][j];
+        }
+
+      }
+
+      const verticalJoin = arr.map(ls => ls.join(''))
+      for (l of verticalJoin) {
+          if (l.includes(word)) return true
+      }
+      return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
